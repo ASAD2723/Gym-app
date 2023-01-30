9a54492510e96317cf19d7ca6f9bb9068c4f8c22
ASAD2723 <khanasad323@gmail.com>
2023-01-31 03:11:13 +0545

    Work in Progress

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+
+const Home = () => {
+const [bodyPart, setbodyPart] = useState('all')
+const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+        <HeroBanner />
+        <SearchExercises 
+            setExercises={setExercises}
+            bodyPart={bodyPart}
+            setBodyPart={setbodyPart}
+        />
+        <Exercises 
+            setExercises={setExercises}
+            bodyPart={bodyPart}
+            setBodyPart={setbodyPart}
+        />
+    </Box>
+  )
+}
+
+export default Home
